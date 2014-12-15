33c7bd2f3d458bc0aeb044818efeb9a98c38ca60
kosmowsp <kosmowsp@ldndwmvdi004238.INTRANET.BARCAPINT.COM>
2014-12-15 17:31:31 +0100

    First version of feature that displays message only when not using keyboard

--- src/main/java/com/github/geertjanwielenga/netbeanskeypromoter/KeyPromoterStatusLineElementProvider.java
+++ src/main/java/com/github/geertjanwielenga/netbeanskeypromoter/KeyPromoterStatusLineElementProvider.java
@@ -1,63 +1,104 @@
 package com.github.geertjanwielenga.netbeanskeypromoter;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.StreamHandler;
 import javax.swing.Action;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import org.openide.awt.Mnemonics;
+import org.openide.awt.NotificationDisplayer;
 import org.openide.awt.StatusLineElementProvider;
-import org.openide.filesystems.FileObject;
 import org.openide.util.lookup.ServiceProvider;
 
 @ServiceProvider(service = StatusLineElementProvider.class, position = 0)
 public class KeyPromoterStatusLineElementProvider implements StatusLineElementProvider {
 
-    private JLabel label;
-    private FileObject fo;
-    
+	private final Logger logger = Logger.getLogger("org.netbeans.ui.actions");
 
-    public KeyPromoterStatusLineElementProvider() {
-        label = new JLabel();
-        Logger logger = Logger.getLogger("org.netbeans.ui.actions");
-        logger.addHandler(new StreamHandler() {
-            @Override
-            public void publish(LogRecord record) {
-                Object[] parameters = record.getParameters();
-                if (parameters[2] instanceof Action) {
-                    Action a = (Action) parameters[2];
-                    JMenuItem menu = new JMenuItem();
-                    Mnemonics.setLocalizedText(
-                            menu,
-                            a.getValue(Action.NAME).toString());
-                    String name = menu.getText();
-                    if (a.getValue(Action.ACCELERATOR_KEY) != null) {
-                        String accelerator = a.getValue(Action.ACCELERATOR_KEY).toString();
-//                        label.setBorder(new TitledBorder(name));
-                        label.setText(
-                                accelerator.replace("pressed", "").replace("alt", "Alt -").replace("ctrl", "Ctrl -").replace("shift", "Shift -") +
-                                        " ("+name.replace("...", "")+")  ");
-//                        NotificationDisplayer.getDefault().notify(
-//                                name,
-//                                new ImageIcon("/org/nb/kp/car.png"),
-//                                accelerator,
-//                                new ActionListener() {
-//                                    @Override
-//                                    public void actionPerformed(ActionEvent e) {
-//                                        a.actionPerformed(e);
-//                                    }
-//                                });
-                    }
-                }
-            }
-        });
-    }
+	private JLabel label;
+	private Action action;
 
-    @Override
-    public Component getStatusLineElement() {
-        return label;
-    }
+	public KeyPromoterStatusLineElementProvider() {
+		label = new JLabel();
+		logger.addHandler(new StreamHandler() {
+			@Override
+			public void publish(LogRecord record) {
+				Object[] parameters = record.getParameters();
+				if (parameters[2] instanceof Action) {
+					action = (Action) parameters[2];
+					JMenuItem menu = new JMenuItem();
+					Mnemonics.setLocalizedText(menu, action.getValue(Action.NAME).toString());
+					if (doesButtonHasShortcut(action) && !isUsingKeyboard()) {
+						String shortcut = getShortcut(action.getValue(Action.ACCELERATOR_KEY).toString());
+						String actionName = getActionName(menu.getText());
+						String message = formatMessage(shortcut, actionName);
+						displayMessage(message);
+					}
+				}
+			}
+
+			private boolean doesButtonHasShortcut(Action a) {
+				return a.getValue(Action.ACCELERATOR_KEY) != null;
+			}
+
+			private String formatMessage(String shortcut, String actionName) {
+				return String.format("%s (%s)", shortcut, actionName);
+			}
+
+			private String getShortcut(String shortcut) {
+				return shortcut
+						.replace("pressed", "")
+						.replace("alt", "Alt -")
+						.replace("ctrl", "Ctrl -")
+						.replace("shift", "Shift -");
+			}
+
+			private String getActionName(String actionName) {
+				return actionName.replace("...", "");
+			}
+
+			private void displayMessage(String message) {
+				if (true) {
+					displayMessageInStatusbar(message);
+				} else {
+					displayMessageInPopupBallon(message);
+				}
+			}
+
+			private boolean isUsingKeyboard() {
+				// bad bad bad, very bad boy, but no other way
+				Thread thread = Thread.currentThread();
+				StackTraceElement[] stackTrace = thread.getStackTrace();
+				StackTraceElement fixedMethodStackTrace = stackTrace[7];
+				String methodName = fixedMethodStackTrace.getMethodName();
+				boolean startsWithProcessKeyBinding = methodName.startsWith("processKeyBinding");
+				return startsWithProcessKeyBinding;
+			}
+		});
+	}
+
+	private void displayMessageInPopupBallon(String message) {
+		NotificationDisplayer.getDefault().notify("You could use shortcut instead", new ImageIcon("/org/nb/kp/car.png"), message, new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				action.actionPerformed(e);
+			}
+		});
+	}
+
+	private void displayMessageInStatusbar(String message) {
+		label.setText(message);
+	}
+
+	@Override
+	public Component getStatusLineElement() {
+		return label;
+	}
 
 }
